4c532d2ed08d3d46e54a2674fb846c8a135f667d
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-28 22:03:57 +0000

    updating ch2 js file

--- Ch02_FileSystemOperation/script.js
+++ Ch02_FileSystemOperation/script.js
@@ -1,1 +1,14 @@
-console.log("JavaScript");
+const fs = require('fs');
+
+/*
+    writefile
+    appendfile
+    copyfile
+    rename
+    unlink
+*/
+
+fs.writeFile("hey.txt", "hey JS learning journey", function(err) {
+    if (err) console.error(err);
+    else console.log("done");
+});
