8c4f725da9b243749bae68db29b738adf3415a4d
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-28 22:15:14 +0000

    fs.unlink is implemented to remove the file

--- Ch02_FileSystemOperation/script.js
+++ Ch02_FileSystemOperation/script.js
@@ -4,12 +4,23 @@ const fs = require('fs');
     fs.writeFile
     fs.appendFile
     fs.rename
-    copyfile
+    fs.copyfile
     rename
-    unlink
+    fs.unlink
 */
 
-fs.rename("hey.txt", "renamedFile.txt", function(err) {
+// fs.rename("hey.txt", "renamedFile.txt", function(err) {
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+
+
+// fs.copyFile("renamed.txt", "copied.txt", function(err) {
+//     if (err) console.error(err);
+//     else console.log("done");
+// })   this code gives you an error, because there is no 'copied.txt' file exists
+
+fs.unlink("renamedFile.txt", function(err) {
     if (err) console.error(err);
     else console.log("done");
 })
