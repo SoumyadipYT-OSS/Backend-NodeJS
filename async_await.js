dc610daf6e85048f5dc5208934f7be174947421f
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-28 21:24:47 +0000

    updating async await concept in js

--- async_await.js
+++ async_await.js
@@ -1,3 +1,18 @@
 async function abcd() {
-    await fetch()
+    let raw = await fetch(`https://randomuser.me/api/`);
+    let ans = await raw.json();
+    console.log(ans);
 }
+
+
+abcd();
+
+
+/* Use cases in real world  */
+// node mein jab DB
+// fetch
+// settimeout
+// setinterval
+
+
+// [concept aside]
