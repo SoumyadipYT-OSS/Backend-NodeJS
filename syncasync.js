6d8151acfdefed25b6bb435a1c3297a0a46a4f4d
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-28 21:05:38 +0000

    updated js code

--- syncasync.js
+++ syncasync.js
@@ -1,2 +1,24 @@
-/*  Synchronous  */
-alert("js");
+/*  
+    User will ask for number between 0 - 9,
+    if the number below to 5, resolve if not reject
+*/
+
+// Promise is a constructor, that's why we use 'new' keyword.
+
+var ans = new Promise((res, rej) => {
+    var n = Math.floor(Math.random()*10);
+
+    if (n < 5) {
+        return res();
+    } else {
+        return rej();
+    }
+})
+
+ans
+    .then(function() {
+        console.log("below");
+    })
+    .catch(function() {
+        console.log("above");
+    })
