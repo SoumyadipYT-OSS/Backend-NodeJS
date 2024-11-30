15cc0b8c619fcddaf39156fd4361d8b575d313cc
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-30 04:29:07 +0000

    Ch03 is updated

---
+++ Ch03_NPM_Basics_andAdvance/script.js
@@ -0,0 +1,14 @@
+// NPM understanding
+
+// install -> npm i packageName
+// uninstall -> npm uninstall packageName
+// specific version install -> npm i packageName@version [example: package@1.0.0]
+
+
+// Understanding node_modules
+
+// dependencies -  packages download (install dependencies packages)
+// devdependencies - download only packages which supports only development and environmnet
+//                      during only development, after publishing the app,
+//                      those dependencies will not going to use.
+
