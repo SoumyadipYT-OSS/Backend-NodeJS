b7e11f5ab1e3cc74eef97f1706b209c6c4bc7ef6
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-11-30 05:05:33 +0000

    learning routing

--- Ch04_Express_Routing_Middleware/script.js
+++ Ch04_Express_Routing_Middleware/script.js
@@ -7,8 +7,13 @@
 const express = require('express');
 const app = express();
 
+// app.get("/", requestHandler) --> requestHandler is a middleware, it is always a function
 app.get('/', function (req, res) {
     res.send('Hello World');
+})
+
+app.get("/profile", function (req, res) {
+    res.send("profile page");
 })
 
 app.listen(3000);
