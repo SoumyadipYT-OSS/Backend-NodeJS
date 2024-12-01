83cb3b38bc59944bb438627bc178c94d800b685b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-12-01 22:37:33 +0000

    code script is updated

--- Ch05_FormHandling_Sessions_Cookies/script.js
+++ Ch05_FormHandling_Sessions_Cookies/script.js
@@ -10,4 +10,21 @@
 // handle backend process of forms and making sure the data coming 
 // from any frontend lib, fw, templating engines, we still handle it at the backend
 
-const express = require()
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+// urlencoded: it is a built-in middleware function
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.send("index page hain yee");
+});
+
+app.get("/about", function(res, req) {
+    res.send("about page hain yein");
+});
+
+app.get("/profile", function(req, res, next) {
+    return next(new Error("Not implemented"));
+});
