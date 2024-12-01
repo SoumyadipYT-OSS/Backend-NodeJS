816dfb4191852d5d6699de47e6a0e0a1723f40e4
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-12-01 22:33:06 +0000

    express is installed in ch05

---
+++ Ch05_FormHandling_Sessions_Cookies/script.js
@@ -0,0 +1,13 @@
+/*
+    session cookie
+        
+    hum log kuch bhi data frontend par browser par rakh skte hain
+    and jab bhi aap kuchh bhi request backend par karoge wo fe par 
+    saved data automatically backend par chala jaayega.
+*/
+
+// form handling and working with the forms
+// handle backend process of forms and making sure the data coming 
+// from any frontend lib, fw, templating engines, we still handle it at the backend
+
+const express = require()
