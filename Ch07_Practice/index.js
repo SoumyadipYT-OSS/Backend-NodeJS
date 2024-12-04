cf9fb7546a3f4f856b8c68857a478171a1e73cd9
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-12-04 23:43:49 +0000

    Practice session Ch07 fully completed

--- Ch07_Practice/index.js
+++ Ch07_Practice/index.js
@@ -14,6 +14,16 @@ app.get('/', function(req, res) {
     });
 });
 
+
+app.get('/file/:filename', function(req, res) {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata) {
+        if (err) {
+            return res.status(500).send("Error reading file");
+        }
+        res.render('show', { filename: req.params.filename, filedata: filedata });
+    });
+});
+
 app.post('/create', function(req, res) {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err) {
         res.redirect("/");
