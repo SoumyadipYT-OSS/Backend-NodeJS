46fb0ee92b34c499b0f555092f263f4019c8b3de
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-12-04 22:38:15 +0000

    fs moduled is improted and fs.readdir is used and implemented path and a callback function as two parameters

--- Ch07_Practice/index.js
+++ Ch07_Practice/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
+    fs.readdir(`./files`, function(err, files) {
+        console.log(files);
+    })
     res.render("index");
 });
 
